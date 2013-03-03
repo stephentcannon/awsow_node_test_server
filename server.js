c1cea0edb984052149fbbf9e0c2f4215397ae94b
Stephen T Cannon <stephentcannon@gmail.com>
2013-03-03 09:40:16 -0500

    'init'

---
+++ server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World! From node http server res.edn! \n');
+}).listen(80);
