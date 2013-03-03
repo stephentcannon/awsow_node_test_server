7903a2b9460472775d24913bb93323b1e6bab117
Stephen T Cannon <stephentcannon@gmail.com>
2013-03-03 10:06:09 -0500

    'doh!'

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! From node http server! \n
-  	This line tested a redeploy from awsow pulling from git. \n
-  	');
+  res.end('Hello World! From node http server! \nThis line tested a redeploy from awsow pulling from git. \n');
 }).listen(80);
